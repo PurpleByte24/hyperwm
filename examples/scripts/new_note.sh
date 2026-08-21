23beb1d2caf188e16ede8d48f3a2825a246b5985
purplebyte24 <purplebyte24+gitea-user@gmail.com>
2026-08-21 16:18:17 +0200

    Add example scripts referenced by examples/config.toml's keybinds.scripts

---
+++ examples/scripts/new_note.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Example hyper+n script referenced by examples/config.toml.
+open -na "Notes"
---
+++ examples/scripts/toggle_ghostty.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Example hyper+g script referenced by examples/config.toml.
+open -a Ghostty
